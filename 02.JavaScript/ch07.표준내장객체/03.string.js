c4ec42d0acdca82bef63fe25733c20cc4d59cdb1
Myungho Lee <su757202@gmail.com>
2023-03-21 11:24:43 +0900

    2023.03.21_JS

--- 02.JavaScript/ch07.표준내장객체/03.string.js
+++ 02.JavaScript/ch07.표준내장객체/03.string.js
@@ -79,15 +79,21 @@ console.log('1,1,1'.split(',').length);    // ['1','1','1']
 
 // 1 에서 1000 까지 숫자가 있다.
 // 0은 몇번, 1은 몇번, ..., 9는 몇번 사용되는가?
+function strCount(str, dst) {           // str 에서 dst가 몇번 사용되는가
+    return str.split(dst).length - 1;
+}
 
 let numStr = '';
 for (let i = 1; i<1000; i++)
     numStr += i;                // 1234...9991000
 console.log(numStr.length)
 for(let num = 0; num <= 9; num++) {
-    let count = numStr.split(String(num)).length - 1;
+    // let count = numStr.split(String(num)).length - 1;
+    let count = strCount(numStr, String(num));
     console.log(`${num} 은/는 ${count} 번 사용됨.`);
 }
+
+
 
 // isPalindrome 함수 만들기
 function isPalindrome(str) {
